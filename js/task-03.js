07fd93402872c6cea5ace6d282c682e2647cfbb5
OlkaPasko <olka.pasko@gmail.com>
2021-04-24 19:40:50 +0300

    3rd task

---
+++ js/task-03.js
@@ -0,0 +1,28 @@
+import images from './data/images.js';
+
+// Используй массив объектов images
+// для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+const makeLiElements = images => {
+    return  images.map(({url, alt}) => {
+        const liEl = document.createElement('li');
+        const imgEl = document.createElement('img');
+        
+        imgEl.src = url;
+        imgEl.alt = alt;
+
+        liEl.append(imgEl);
+
+        return liEl;
+    });
+};
+
+const ulEl = document.querySelector('#gallery');
+
+const liElements = makeLiElements(images);
+const liElementsToString = liElements.map(liEl => liEl.outerHTML);
+
+ulEl.insertAdjacentHTML('afterbegin', liElementsToString);
+
+
