1a9517e681689403bfce83b56f9ad2dfa5de4a98
OlkaPasko <olka.pasko@gmail.com>
2021-04-28 14:13:23 +0300

    fixed event type

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output');
 
-inputName.addEventListener('keydown', onKeyboardEvent);
+inputName.addEventListener('input', onKeyboardEvent);
 
 function onKeyboardEvent() {
     outputName.textContent = inputName.value;
